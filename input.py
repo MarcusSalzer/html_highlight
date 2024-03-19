e41d3873c4ec17aea134ea27e4dc696ceef8affe
Marcus <marcus.salzer@hotmail.se>
2024-03-19 23:11:03 +0100

    more tags

--- input.py
+++ input.py
@@ -1,8 +1,9 @@
-from highlighting_functions import Highlighter
 import json as jorre
 
 x = 37
+for k in range(x):
+    print("öh" * k)
+    
 this_variable = "hej på dig"
-trippeljävel = """w"t"f"""
 
-a_longer123string = (this_variable + 'ööhö')
+a_longer123string = this_variable + "ööhö"
