faef59de2fc27c050803c1974b6e13438e5a4dd7
Marcus <marcus.salzer@hotmail.se>
2024-03-29 15:03:32 +0100

    color generation. functions cleanup

--- highlighting_functions.py
+++ highlighting_functions.py
@@ -85,3 +85,156 @@ class Highlighter:
 
         text = self.syntax_highlight(text)
         return text
+
+
+## new functions
+def tokenize(text) -> list[str]:
+    """Tokenize a text"""
+    # Possible string delimiters
+    string_delims = {'"', "'", '"""', "'''"}
+
+    # split everything
+    tokens: list[str] = re.findall(r"(\w+|[^\w\s]+|\s+|\n)", text)
+
+    print(tokens)
+    ## split tokens without letters or numbers
+    tmp: list[str] = []
+    for t in tokens:
+        if bool(re.search(r"\p{L}|\p{digit}", t)):
+            tmp.append(t)
+        # elif len(t) > 1 and all((c == t[0] for c in t)):
+        #     tmp.append(t)
+        else:
+            tmp.extend(list(t))
+
+    tokens = tmp
+
+    ## reunite string literals
+    tmp = []
+    tags = []
+    current_delim = ""
+    current_string = ""
+    for i, t in enumerate(tokens):
+        if current_delim == "":
+            if t in string_delims:
+                # start a string
+                current_delim = t
+                current_string += t
+            else:
+                tmp.append(t)
+                tags.append("unk")
+        else:
+            if t == current_delim:
+                # break current string
+                current_delim = ""
+                current_string += t
+                tmp.append(current_string)
+                tags.append("str")
+                current_string = ""
+            else:
+                # add to current string
+                current_string += t
+
+    tokens = tmp
+
+    ## reunite decimal numbers
+    tmp = []
+    tmp_tags = []
+    current_num = ""
+    for i, (token, tag) in enumerate(zip(tokens, tags)):
+        token_next = None
+        if i < len(tokens) - 1:
+            token_next = tokens[i + 1]
+
+        num_char = token.isdigit() or (token == "." and token_next.isdigit())
+
+        if current_num == "":
+            if num_char:
+                current_num = token
+            else:
+                tmp.append(token)
+                tmp_tags.append(tag)
+        else:
+            if num_char:
+                current_num += token
+            else:
+                # end number
+                tmp.append(current_num)
+                tmp_tags.append("num")
+                current_num = ""
+                tmp.append(token)
+                tmp_tags.append(tag)
+
+    tokens = tmp
+    tags = tmp_tags
+
+    return tokens, tags
+
+
+def tag_individuals(tokens, tags, known_str: dict):
+    """Tag individual tokens based on known symbols/strings"""
+    for i, token in enumerate(tokens):
+        if token.isdigit():
+            tags[i] = "num"
+            continue
+        elif not bool(re.search("[^\s]", token)):
+            tags[i] = "wsp"
+
+        if tags[i] == "unk":
+            for key in known_str.keys():
+                if token in known_str[key]:
+                    tags[i] = key
+                    break
+
+    return tokens, tags
+
+
+def merge_adjacent(tokens, tags):
+    tmp = []
+    tmp_tags = []
+
+    current_seq = []
+    for i, (token, tag) in enumerate(zip(tokens, tags)):
+        # next tag, None if at end
+        tag_next = tags[i + 1] if i < len(tags) - 1 else None
+
+        if not tag == tag_next:
+            if len(current_seq) > 0:
+                current_seq.append(token)
+                tmp.append("".join(current_seq))
+                tmp_tags.append(tag)
+                current_seq = []
+            else:
+                tmp.append(token)
+                tmp_tags.append(tag)
+        else:
+            current_seq.append(token)
+
+    return tmp, tmp_tags
+
+
+def html_specials(text: str) -> str:
+    """Replace html reserved characters, preserve indentation and line-breaks"""
+    text = re.sub(r"&", r"&amp;", text)
+    text = re.sub(r"<", r"&lt;", text)
+    text = re.sub(r">", r"&gt;", text)
+    text = re.sub(r'"', r"&quot;", text)
+    text = re.sub(r"'", r"&apos;", text)
+    text = re.sub(r"  ", r"&nbsp; ", text)
+    text = re.sub(r"\n", r"<br>\n", text)
+    return text
+
+
+def tokens_to_html(tokens, tags, exclude_tags=()):
+    tokens_with_tags = []
+    for token, tag in zip(tokens, tags):
+        # fix html specials
+        token_text = html_specials(token)
+        if tag in exclude_tags:
+            tokens_with_tags.append(token_text)
+        else:
+            tokens_with_tags.append(f"""<span class="{tag}">{token_text}</span>""")
+
+    text = "".join(tokens_with_tags)
+    return f"""<div class="code-snippet">{text}</div>"""
+
